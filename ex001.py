369719ee95b53392a91d7237f3de78d65836ddea
brenoGoya <64437875+brenoGoya@users.noreply.github.com>
2020-06-29 11:24:12 -0300

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,9 @@
+# Challlenge 001
+
+""" Ask for the user's first name and display the output message: """
+
+# Hello [Fisrst Name].
+
+first_name = input("What's your first name? >>> ")
+print(f"Hello {first_name}.")
+
