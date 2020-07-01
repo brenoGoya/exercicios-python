7c899a949a0efd2db7e026ad23be8aa3df6f859b
brenoGoya <64437875+brenoGoya@users.noreply.github.com>
2020-07-01 14:22:26 -0300

    If Statements

--- ex015.py
+++ ex015.py
@@ -1,7 +1,7 @@
 # Challenge 015
 
-""" Ask the user to enter their favourite colour. If they enter "red"
-, "RED" or "Red" display the message "I like red too". otherwise display
+""" Ask the user to enter their favourite colour. If they enter "red",
+"RED" or "Red" display the message "I like red too". otherwise display
 the message "I don't like [colour], I prefer red". """
 
 colour = input('What is your favourite colour?\n>> ').lower()
