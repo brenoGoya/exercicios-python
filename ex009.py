2ddccf7a1087967263c431b03d412f904042a214
brenoGoya <64437875+brenoGoya@users.noreply.github.com>
2020-07-01 14:42:30 -0300

    The Basics

--- ex009.py
+++ ex009.py
@@ -6,7 +6,9 @@ and seconds are in that number of days. """
 number_days = int(input("Type the number of days:\n>> "))
 
 hours = number_days * 24
+
 minutes = hours * 60
+
 seconds = minutes * 60
 
 print(f'In {number_days} days, there are {hours} hours, {minutes} minutes and {seconds} seconds.')
