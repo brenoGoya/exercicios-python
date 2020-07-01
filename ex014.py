d3b045b1ea3dd5c1999feea0c1b5b75f19b56e96
brenoGoya <64437875+brenoGoya@users.noreply.github.com>
2020-07-01 14:19:13 -0300

    If Statements

--- ex014.py
+++ ex014.py
@@ -4,6 +4,7 @@
 this range, display the message "Thank you", otherwise display the message "Incorrect answer". """
 
 number = int(input('Enter a number between 10 and 20:\n>> '))
+
 if 10 <= number <= 20:
     print('Thank you')
 else:
